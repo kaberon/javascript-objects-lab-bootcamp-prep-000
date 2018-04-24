38693d284ac4e7a7a9d97671896ffb81b2e0376b
Ben Kaberon <kaberon@gmail.com>
2018-04-24 13:40:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {pasta:"pound", sauce:"vodka", cheese: "parm"};
+
+
+  function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+  }
+  
+//the above already work
+
+function deleteFromObjectByKey(object, key) {
+  Object.assign({newKey}, obj, { [key]: value })
+  return object;
+}
